9077bde2375bc329f28d88b5fe2d7eb35ff363f7
THEBR4Y4N <garzonbrayan1201@hotmail.com>
2022-03-15 18:11:44 -0500

    fase4

---
+++ MiIngresoHitss/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiIngresoHitss.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripciòn { get; set; }
+        public double Precio { get; set; }
+    }
+}
